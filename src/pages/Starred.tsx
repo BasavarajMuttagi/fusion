847793b6a37a03762ce9bafb8b80dc374a188304
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-08-12 23:30:58 +0530

    integrate task card,  refactor

--- src/pages/Starred.tsx
+++ src/pages/Starred.tsx
@@ -1,4 +1,3 @@
-import { isAxiosError } from "axios";
 import {
   useState,
   useEffect,
@@ -6,14 +5,10 @@ import {
   Dispatch,
   SetStateAction,
 } from "react";
-import toast from "react-hot-toast";
-import apiClient from "../axios/apiClient";
 import Tabs from "../components/Tabs";
-import ContentAwareVideo from "../components/ContentAwareVideo";
-import moment from "moment";
 import { CloudinaryAsset } from "../types";
-import StarButton from "../components/StarButton";
 import { getStarredAssets } from "../axios/apiService";
+import AssetCard from "../components/AssetCard";
 
 export const ActiveTabContextStarred = createContext<
   [activeTabType, Dispatch<SetStateAction<activeTabType>>]
@@ -24,17 +19,6 @@ const Starred = () => {
   const [activeTab, setActiveTab] = useState<activeTabType>("IMAGE");
   const [recentImages, setRecentImages] = useState<CloudinaryAsset[]>();
   const [recentVideos, setRecentVideos] = useState<CloudinaryAsset[]>();
-
-  const deleteById = async (assetId: string) => {
-    try {
-      const res = await apiClient.get(`/cloudinary/delete/${assetId}`);
-      return res;
-    } catch (error) {
-      if (isAxiosError(error)) {
-        toast.error(error.message);
-      }
-    }
-  };
 
   useEffect(() => {
     getStarredAssets("image").then((res) => {
@@ -65,50 +49,9 @@ const Starred = () => {
                 </span>
               </h2>
               <div className="grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-                {recentImages?.map(
-                  ({
-                    secureUrl,
-                    createdAt,
-                    height,
-                    width,
-                    format,
-                    resourceType,
-                    assetId,
-                    displayName,
-                    starred,
-                  }) => {
-                    return (
-                      <div
-                        className="border p-2 rounded-md space-y-1 shadow"
-                        key={secureUrl}
-                      >
-                        <img src={secureUrl} className="w-full aspect-square" />
-                        <div className="space-y-2">
-                          <div className="text-sm font-semibold lg:text-base">
-                            {displayName}
-                          </div>
-                          <div className="space-y-2 text-gray-600 text-[8px] font-medium md:text-[9px] xl:text-[10px]">
-                            <div>Format : {`${resourceType}/${format}`}</div>
-                            <div>Dimensions: {`${height} X ${width}`}</div>
-                            <div>
-                              Created At :{" "}
-                              {moment(createdAt).format(
-                                "MMMM Do YYYY, h:mm:ss a",
-                              )}
-                            </div>
-                            <button
-                              type="button"
-                              onClick={() => deleteById(assetId)}
-                            >
-                              Delete
-                            </button>
-                            <StarButton assetId={assetId} starred={starred} />
-                          </div>
-                        </div>
-                      </div>
-                    );
-                  },
-                )}
+                {recentImages?.map((eachImage) => {
+                  return <AssetCard {...eachImage} />;
+                })}
               </div>
             </div>
           )}
@@ -121,54 +64,9 @@ const Starred = () => {
                 </span>
               </h2>
               <div className="grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-                {recentVideos?.map(
-                  ({
-                    publicId,
-                    secureUrl,
-                    createdAt,
-                    height,
-                    width,
-                    format,
-                    resourceType,
-                    displayName,
-                    assetId,
-                    starred,
-                  }) => {
-                    return (
-                      <div
-                        className="border p-2 rounded-md space-y-1 shadow"
-                        key={secureUrl}
-                      >
-                        <ContentAwareVideo
-                          cloudName={import.meta.env.VITE_CLOUDINARY_NAME}
-                          publicId={publicId}
-                        />
-                        <div className="space-y-2">
-                          <div className="text-sm font-semibold lg:text-base">
-                            {displayName}
-                          </div>
-                          <div className="space-y-2 text-gray-600 text-[8px] font-medium md:text-[9px] xl:text-[10px]">
-                            <div>Format : {`${resourceType}/${format}`}</div>
-                            <div>Dimensions: {`${height} X ${width}`}</div>
-                            <div>
-                              Created At :{" "}
-                              {moment(createdAt).format(
-                                "MMMM Do YYYY, h:mm:ss a",
-                              )}
-                            </div>
-                            <button
-                              type="button"
-                              onClick={() => deleteById(assetId)}
-                            >
-                              Delete
-                            </button>
-                            <StarButton assetId={assetId} starred={starred} />
-                          </div>
-                        </div>
-                      </div>
-                    );
-                  },
-                )}
+                {recentVideos?.map((eachVideo) => {
+                  return <AssetCard {...eachVideo} />;
+                })}
               </div>
             </div>
           )}
